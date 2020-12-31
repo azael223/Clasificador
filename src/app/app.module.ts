295babb42fcb8c9136cb1207c220fe8d6943860f
Azael Rivera <azaelkiller@gmail.com>
2020-12-30 19:56:49 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormModule } from './form/form.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { FormModule } from './form/form.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -11,12 +11,28 @@ export class FormComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(private _fb: FormBuilder) {}
 
   public form = this._fb.group({
-    dataset: new FormControl('', []),
-    datasetext: new FormControl('', []),
-    inDis: new FormControl('', []),
+    dataset: new FormControl('', [Validators.required]),
+    datasetExt: new FormControl('', []),
+    readType: new FormControl('MD', [Validators.required]),
+    validationType: new FormControl('VS', []),
+    inDis: new FormControl('', [Validators.required]),
   });
 
   private onDestroy = new Subject<any>();
+
+  readTypeChange() {
+    const readType = this.form.get('readType');
+    console.log(readType.value)
+    const dataExt = this.form.get('datasetExt');
+    const validationType = this.form.get('validationType');
+    if (readType.value === 'AE') {
+      dataExt.setValidators(Validators.required);
+      validationType.clearValidators();
+    } else {
+      validationType.setValidators(Validators.required);
+      dataExt.clearValidators();
+    }
+  }
 
   ngOnInit(): void {}
 
